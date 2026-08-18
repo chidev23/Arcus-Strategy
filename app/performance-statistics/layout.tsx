b92496beeefbc2f20bf2899d437ea9fbdfacaac4
chidev23 <chidi.dev.coder@gmail.com>
2026-08-18 14:17:08 +0100

    Add Myfxbook CTA to broker performance statistics pages

---
+++ app/performance-statistics/layout.tsx
@@ -0,0 +1,5 @@
+import MyfxbookCTA from '../../components/MyfxbookCTA';
+
+export default function PerformanceStatisticsLayout({children}:{children:React.ReactNode}){
+  return <>{children}<MyfxbookCTA /></>;
+}
