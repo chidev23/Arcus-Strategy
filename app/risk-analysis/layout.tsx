a1f2f558690f5ae142599c8530a2d0a946e7be19
chidev23 <chidi.dev.coder@gmail.com>
2026-08-18 14:17:16 +0100

    Add Myfxbook CTA to broker risk analysis pages

---
+++ app/risk-analysis/layout.tsx
@@ -0,0 +1,5 @@
+import MyfxbookCTA from '../../components/MyfxbookCTA';
+
+export default function RiskAnalysisLayout({children}:{children:React.ReactNode}){
+  return <>{children}<MyfxbookCTA /></>;
+}
