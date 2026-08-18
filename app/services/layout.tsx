1ba236ad2e6e9199bc70c3673e2581dd2ad6affc
chidev23 <chidi.dev.coder@gmail.com>
2026-08-18 14:16:58 +0100

    Add Myfxbook CTA to broker service pages

--- app/services/layout.tsx
+++ app/services/layout.tsx
@@ -1,2 +1,6 @@
 import RequireAuth from '../../components/RequireAuth';
-export default function ServicesLayout({children}:{children:React.ReactNode}){return <RequireAuth>{children}</RequireAuth>}
+import MyfxbookCTA from '../../components/MyfxbookCTA';
+
+export default function ServicesLayout({children}:{children:React.ReactNode}){
+  return <RequireAuth><>{children}<MyfxbookCTA /></></RequireAuth>;
+}
